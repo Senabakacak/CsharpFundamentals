589d82e9626b792477acb96a8a8dbb71be5a6166
sena bakacak <senabakacak2@hotmail.com>
2022-01-24 22:40:51 +0300

    for,arrays

---
+++ For-Loops/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace For_Loops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexample1 = new System.Windows.Forms.Button();
+            this.btnexample2 = new System.Windows.Forms.Button();
+            this.btnexample3 = new System.Windows.Forms.Button();
+            this.btnexample4 = new System.Windows.Forms.Button();
+            this.btnexample7 = new System.Windows.Forms.Button();
+            this.btnexample5 = new System.Windows.Forms.Button();
+            this.btnexample9 = new System.Windows.Forms.Button();
+            this.btnexample8 = new System.Windows.Forms.Button();
+            this.btnexample6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtend = new System.Windows.Forms.TextBox();
+            this.txtstart = new System.Windows.Forms.TextBox();
+            this.txtbrand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnexample1
+            // 
+            this.btnexample1.Location = new System.Drawing.Point(30, 24);
+            this.btnexample1.Name = "btnexample1";
+            this.btnexample1.Size = new System.Drawing.Size(119, 62);
+            this.btnexample1.TabIndex = 0;
+            this.btnexample1.Text = "Example 1";
+            this.btnexample1.UseVisualStyleBackColor = true;
+            this.btnexample1.Click += new System.EventHandler(this.btnexample1_Click);
+            // 
+            // btnexample2
+            // 
+            this.btnexample2.Location = new System.Drawing.Point(30, 101);
+            this.btnexample2.Name = "btnexample2";
+            this.btnexample2.Size = new System.Drawing.Size(119, 62);
+            this.btnexample2.TabIndex = 1;
+            this.btnexample2.Text = "Example 2";
+            this.btnexample2.UseVisualStyleBackColor = true;
+            this.btnexample2.Click += new System.EventHandler(this.btnexample2_Click);
+            // 
+            // btnexample3
+            // 
+            this.btnexample3.Location = new System.Drawing.Point(166, 20);
+            this.btnexample3.Name = "btnexample3";
+            this.btnexample3.Size = new System.Drawing.Size(119, 67);
+            this.btnexample3.TabIndex = 2;
+            this.btnexample3.Text = "Example 3";
+            this.btnexample3.UseVisualStyleBackColor = true;
+            this.btnexample3.Click += new System.EventHandler(this.btnexample3_Click);
+            // 
+            // btnexample4
+            // 
+            this.btnexample4.Location = new System.Drawing.Point(166, 100);
+            this.btnexample4.Name = "btnexample4";
+            this.btnexample4.Size = new System.Drawing.Size(119, 63);
+            this.btnexample4.TabIndex = 3;
+            this.btnexample4.Text = "Example 4";
+            this.btnexample4.UseVisualStyleBackColor = true;
+            this.btnexample4.Click += new System.EventHandler(this.btnexample4_Click);
+            // 
+            // btnexample7
+            // 
+            this.btnexample7.Location = new System.Drawing.Point(324, 297);
+            this.btnexample7.Name = "btnexample7";
+            this.btnexample7.Size = new System.Drawing.Size(115, 58);
+            this.btnexample7.TabIndex = 4;
+            this.btnexample7.Text = "Example 7";
+            this.btnexample7.UseVisualStyleBackColor = true;
+            this.btnexample7.Click += new System.EventHandler(this.btnexample7_Click);
+            // 
+            // btnexample5
+            // 
+            this.btnexample5.Location = new System.Drawing.Point(170, 297);
+            this.btnexample5.Name = "btnexample5";
+            this.btnexample5.Size = new System.Drawing.Size(115, 62);
+            this.btnexample5.TabIndex = 5;
+            this.btnexample5.Text = "Examples 5";
+            this.btnexample5.UseVisualStyleBackColor = true;
+            this.btnexample5.Click += new System.EventHandler(this.btnexample5_Click);
+            // 
+            // btnexample9
+            // 
+            this.btnexample9.Location = new System.Drawing.Point(459, 25);
+            this.btnexample9.Name = "btnexample9";
+            this.btnexample9.Size = new System.Drawing.Size(115, 62);
+            this.btnexample9.TabIndex = 6;
+            this.btnexample9.Text = "Examples 9";
+            this.btnexample9.UseVisualStyleBackColor = true;
+            this.btnexample9.Click += new System.EventHandler(this.btnexample9_Click);
+            // 
+            // btnexample8
+            // 
+            this.btnexample8.Location = new System.Drawing.Point(324, 100);
+            this.btnexample8.Name = "btnexample8";
+            this.btnexample8.Size = new System.Drawing.Size(115, 62);
+            this.btnexample8.TabIndex = 7;
+            this.btnexample8.Text = "Examples 8";
+            this.btnexample8.UseVisualStyleBackColor = true;
+            this.btnexample8.Click += new System.EventHandler(this.btnexample8_Click);
+            // 
+            // btnexample6
+            // 
+            this.btnexample6.Location = new System.Drawing.Point(324, 22);
+            this.btnexample6.Name = "btnexample6";
+            this.btnexample6.Size = new System.Drawing.Size(115, 63);
+            this.btnexample6.TabIndex = 8;
+            this.btnexample6.Text = "Examples 6";
+            this.btnexample6.UseVisualStyleBackColor = true;
+            this.btnexample6.Click += new System.EventHandler(this.btnexample6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(580, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 404);
+            this.listBox1.TabIndex = 9;
+            // 
+            // txtend
+            // 
+            this.txtend.Location = new System.Drawing.Point(214, 263);
+            this.txtend.Name = "txtend";
+            this.txtend.Size = new System.Drawing.Size(71, 26);
+            this.txtend.TabIndex = 10;
+            // 
+            // txtstart
+            // 
+            this.txtstart.Location = new System.Drawing.Point(214, 224);
+            this.txtstart.Name = "txtstart";
+            this.txtstart.Size = new System.Drawing.Size(71, 26);
+            this.txtstart.TabIndex = 11;
+            // 
+            // txtbrand
+            // 
+            this.txtbrand.Location = new System.Drawing.Point(324, 265);
+            this.txtbrand.Name = "txtbrand";
+            this.txtbrand.Size = new System.Drawing.Size(115, 26);
+            this.txtbrand.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "end";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "brand";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbrand);
+            this.Controls.Add(this.txtstart);
+            this.Controls.Add(this.txtend);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnexample6);
+            this.Controls.Add(this.btnexample8);
+            this.Controls.Add(this.btnexample9);
+            this.Controls.Add(this.btnexample5);
+            this.Controls.Add(this.btnexample7);
+            this.Controls.Add(this.btnexample4);
+            this.Controls.Add(this.btnexample3);
+            this.Controls.Add(this.btnexample2);
+            this.Controls.Add(this.btnexample1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnexample1;
+        private System.Windows.Forms.Button btnexample2;
+        private System.Windows.Forms.Button btnexample3;
+        private System.Windows.Forms.Button btnexample4;
+        private System.Windows.Forms.Button btnexample7;
+        private System.Windows.Forms.Button btnexample5;
+        private System.Windows.Forms.Button btnexample9;
+        private System.Windows.Forms.Button btnexample8;
+        private System.Windows.Forms.Button btnexample6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtend;
+        private System.Windows.Forms.TextBox txtstart;
+        private System.Windows.Forms.TextBox txtbrand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ For-Loops/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace For_Loops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnexample1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void btnexample2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                else
+                {
+                    listBox1.Items.Add(i);
+                }
+
+
+            }
+        }
+
+        private void btnexample3_Click(object sender, EventArgs e)
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+                listBox1.Items.Add($"{c}--->{Convert.ToInt32(c)}");
+        }
+
+        private void btnexample4_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                listBox1.Items.Add("pink.com");
+            }
+        }
+
+        private void btnexample5_Click(object sender, EventArgs e)
+        {
+            int start = Convert.ToInt32(txtstart.Text);
+            int end = Convert.ToInt32(txtend.Text);
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox1.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void btnexample6_Click(object sender, EventArgs e)
+        {
+            //Arrays
+            string[] brands = { "trenyol", "shein", "amazon", "zara" };
+            MessageBox.Show(brands[0].ToString());
+            for (int i = 0; i < brands.Length; i++)
+            {
+                listBox1.Items.Add(brands[i]);
+            }
+        }
+
+        private void btnexample7_Click(object sender, EventArgs e)
+        {
+            string[] brands = { "bershka", "stradivarius", "beymen", "mavi", "pink" };
+            string result = string.Empty;
+            bool condition = false;
+
+            for (int i = 0; i < brands.Length; i++)
+            {
+                if (txtbrand.Text == brands[i])
+                {
+                    result = brands[i];
+                    condition = true;
+                    break;
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        private void btnexample8_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int[] numbers_1 = new int[10];
+            int[] numbers_2 = new int[10];
+
+            for (int i = 0; i < numbers_1.Length; i++)
+            {
+                numbers_1[i] = random.Next(1, 101);
+                numbers_2[i] = random.Next(1, 101);
+                listBox1.Items.Add($"{numbers_1[i]}+{numbers_2[i]}={numbers_1[i] + numbers_2[i]}");
+            }
+        }
+
+        private void btnexample9_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.Controls.Count; i++)
+            {
+                if (this.Controls[i] is Button)
+                {
+                    this.Controls[i].BackColor = Color.Pink;
+                    this.Controls[i].Width = 100;
+                }
+                else if (this.Controls[i] is ListBox)
+                {
+                    this.Controls[i].BackColor = Color.AliceBlue;
+                    listBox1.Font = new Font("tahoma", 15);
+                }
+                else if (this.Controls[i] is TextBox)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
