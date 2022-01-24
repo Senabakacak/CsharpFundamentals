5e742b64318f0029cd95ea50f59c0d91190f3101
sena bakacak <senabakacak2@hotmail.com>
2022-01-24 22:23:18 +0300

    examples

---
+++ DowhileLoops/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DowhileLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExample2 = new System.Windows.Forms.Button();
+            this.btnExample1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExample2
+            // 
+            this.btnExample2.Location = new System.Drawing.Point(53, 138);
+            this.btnExample2.Name = "btnExample2";
+            this.btnExample2.Size = new System.Drawing.Size(156, 69);
+            this.btnExample2.TabIndex = 0;
+            this.btnExample2.Text = "Example 2";
+            this.btnExample2.UseVisualStyleBackColor = true;
+            this.btnExample2.Click += new System.EventHandler(this.btnExample2_Click);
+            // 
+            // btnExample1
+            // 
+            this.btnExample1.Location = new System.Drawing.Point(53, 40);
+            this.btnExample1.Name = "btnExample1";
+            this.btnExample1.Size = new System.Drawing.Size(156, 76);
+            this.btnExample1.TabIndex = 1;
+            this.btnExample1.Text = "Example 1";
+            this.btnExample1.UseVisualStyleBackColor = true;
+            this.btnExample1.Click += new System.EventHandler(this.btnExample1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(252, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(189, 364);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExample1);
+            this.Controls.Add(this.btnExample2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExample2;
+        private System.Windows.Forms.Button btnExample1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DowhileLoops/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DowhileLoops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExample1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            do
+            {
+                listBox1.Items.Add(i);
+                i++;
+            } while (i > 100);
+        }
+
+        private void btnExample2_Click(object sender, EventArgs e)
+        {
+            //1-250 arasındaki çift ve tek sayıların toplamının karesini bulun
+            // Find the square of the sum of the even and odd numbers from /1-250
+            //odd number total= tek sayı toplamı
+            //even number total=çift sayı toplamı
+            int i = 0;
+            int oddnumbertotal = 0;
+            int evennumbertotal = 0;
+
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    evennumbertotal += i;
+                }
+                else
+                {
+                    oddnumbertotal += i;
+                }
+                i++;
+            } while (i <= 250);
+            int result = (evennumbertotal + oddnumbertotal) * (evennumbertotal + oddnumbertotal);
+            listBox1.Items.Add($"even number total:{evennumbertotal}");
+            listBox1.Items.Add($"odd number total:{oddnumbertotal}");
+        }
+    }
+}
