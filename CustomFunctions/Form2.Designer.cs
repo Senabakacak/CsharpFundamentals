1bcf656c747926ba3553d62a572be634c16afd2a
sena bakacak <senabakacak2@hotmail.com>
2022-01-25 21:42:31 +0300

    example

---
+++ CustomFunctions/Form2.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace CustomFunctions
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsignup = new System.Windows.Forms.Button();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.txtcaptcha = new System.Windows.Forms.TextBox();
+            this.txtsigninpassword = new System.Windows.Forms.TextBox();
+            this.txtsigninusername = new System.Windows.Forms.TextBox();
+            this.lblcaptcha = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnsignup);
+            this.groupBox1.Controls.Add(this.txtpassword);
+            this.groupBox1.Controls.Add(this.txtusername);
+            this.groupBox1.Controls.Add(this.txtlastname);
+            this.groupBox1.Controls.Add(this.txtfirstname);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(47, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 352);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SIGN UP";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(135, 220);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(166, 26);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(135, 169);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(166, 26);
+            this.txtusername.TabIndex = 2;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(135, 116);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(166, 26);
+            this.txtlastname.TabIndex = 3;
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Location = new System.Drawing.Point(135, 68);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(166, 26);
+            this.txtfirstname.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FIRST  NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "LAST NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "USER NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PASSWORD";
+            // 
+            // btnsignup
+            // 
+            this.btnsignup.Location = new System.Drawing.Point(135, 273);
+            this.btnsignup.Name = "btnsignup";
+            this.btnsignup.Size = new System.Drawing.Size(166, 49);
+            this.btnsignup.TabIndex = 9;
+            this.btnsignup.Text = "Sign Up";
+            this.btnsignup.UseVisualStyleBackColor = true;
+            this.btnsignup.Click += new System.EventHandler(this.btnsignup_Click);
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.ForeColor = System.Drawing.Color.Fuchsia;
+            this.btnSignIn.Location = new System.Drawing.Point(128, 273);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(166, 49);
+            this.btnSignIn.TabIndex = 10;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            // 
+            // txtcaptcha
+            // 
+            this.txtcaptcha.Location = new System.Drawing.Point(128, 220);
+            this.txtcaptcha.Name = "txtcaptcha";
+            this.txtcaptcha.Size = new System.Drawing.Size(166, 26);
+            this.txtcaptcha.TabIndex = 11;
+            // 
+            // txtsigninpassword
+            // 
+            this.txtsigninpassword.Location = new System.Drawing.Point(128, 122);
+            this.txtsigninpassword.Name = "txtsigninpassword";
+            this.txtsigninpassword.Size = new System.Drawing.Size(166, 26);
+            this.txtsigninpassword.TabIndex = 12;
+            // 
+            // txtsigninusername
+            // 
+            this.txtsigninusername.Location = new System.Drawing.Point(128, 65);
+            this.txtsigninusername.Name = "txtsigninusername";
+            this.txtsigninusername.Size = new System.Drawing.Size(166, 26);
+            this.txtsigninusername.TabIndex = 13;
+            // 
+            // lblcaptcha
+            // 
+            this.lblcaptcha.AutoSize = true;
+            this.lblcaptcha.Location = new System.Drawing.Point(134, 175);
+            this.lblcaptcha.Name = "lblcaptcha";
+            this.lblcaptcha.Size = new System.Drawing.Size(0, 20);
+            this.lblcaptcha.TabIndex = 14;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.lblcaptcha);
+            this.groupBox2.Controls.Add(this.txtsigninusername);
+            this.groupBox2.Controls.Add(this.txtsigninpassword);
+            this.groupBox2.Controls.Add(this.txtcaptcha);
+            this.groupBox2.Controls.Add(this.btnSignIn);
+            this.groupBox2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.groupBox2.Location = new System.Drawing.Point(414, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(332, 352);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SIGN IN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(34, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "CAPTCHA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label7.Location = new System.Drawing.Point(8, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "PASSWORD";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label8.Location = new System.Drawing.Point(6, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "USER NAME";
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.ForeColor = System.Drawing.Color.Red;
+            this.btnDictionary.Location = new System.Drawing.Point(47, 428);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(328, 73);
+            this.btnDictionary.TabIndex = 10;
+            this.btnDictionary.Text = "DICTIONARY TEST";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 513);
+            this.Controls.Add(this.btnDictionary);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnsignup;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.TextBox txtcaptcha;
+        private System.Windows.Forms.TextBox txtsigninpassword;
+        private System.Windows.Forms.TextBox txtsigninusername;
+        private System.Windows.Forms.Label lblcaptcha;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDictionary;
+    }
+}
---
+++ CustomFunctions/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomFunctions
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        string GeneratedCaptcha()
+        {
+            string captcha = Guid.NewGuid().ToString();
+            string[] splitedcaptcha = captcha.Split('-');
+            return splitedcaptcha[1].ToString();
+
+        }
+
+        bool Eraser(GroupBox groupBox)
+        {
+            foreach (Control item in groupBox.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = String.Empty;
+                }
+
+            }
+            return true;
+        }
+
+
+        Dictionary<string, string> users = new
+        Dictionary<string, string>();
+
+        bool SignUp(string username, string password)
+        {
+            if (username == " " && password == " ")
+            {
+                return false;
+            }
+            else
+            {
+                users.Add(username, password);
+                return true;
+            }
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            groupBox2.Visible = false;
+            lblcaptcha.Text = GeneratedCaptcha();
+        }
+
+        private void btnsignup_Click(object sender, EventArgs e)
+        {
+            bool result = SignUp(txtusername.Text, txtpassword.Text);
+
+            if(result==true)
+            {
+                groupBox2.Visible = true;
+                Eraser(groupBox1);
+            }
+            else
+            {
+                MessageBox.Show("lütfen bilgilerinizi giriniz..");
+            }
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<users.Count;i++)
+            {
+                MessageBox.Show($"user name:{users.ElementAt(i).Key}\n password: {users.ElementAt(i).Value}");
+            }
+        }
+    }
+    
+}
+
